8a18e268288c2450b5ece87a5910f48ae5f268f3
Gaston Barbaccia <gastonbarbaccia@devcodeacademy.com>
2025-03-02 20:59:43 -0300

    ok

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@
 sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt-get install docker.io -y
-sudo docker run -it --rm --network=host tleemcjr/metasploitable2
+sudo docker run -it --rm --network=host -d --name metasploitable2 tleemcjr/metasploitable2
